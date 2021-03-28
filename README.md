b05a1ce2f110f544e784fdd6b9aed986e7f1980d
Andy Yu <andyyu2004@gmail.com>
2021-03-28 20:35:07 +1300

    init, roughly working as one may expect?

---
+++ README.md
@@ -0,0 +1,6 @@
+# vscode-fzf README
+
+Early attempt at emulating a rg fuzzy finder such as fzf.vim's :Rg or
+telescope.nvim's live_grep.
+
+Exposes the command `vscode-fzf.rg`
---
+++ src/extension.ts
@@ -0,0 +1,150 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import * as cp from "child_process";
+import * as path from "path";
+import { workspace, window, QuickPickItem } from "vscode";
+import { Readable } from "node:stream";
+
+const LIMIT = 5000;
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "vscode-fzf.rg",
+    async () => {
+      const quickPick = window.createQuickPick<Item>();
+      quickPick.matchOnDescription = false;
+      quickPick.matchOnDetail = true;
+      quickPick.placeholder = "<search>";
+      quickPick.items = await populateSearchItems();
+
+      const originalEditor = window.activeTextEditor;
+      const originalDoc = originalEditor?.document;
+      const originalSelection = originalEditor?.selection;
+      console.log(originalEditor?.document.fileName);
+
+      let hasAccepted = false;
+
+      quickPick.onDidAccept(async () => {
+        hasAccepted = true;
+        showDocument(quickPick.selectedItems[0]);
+      });
+
+      quickPick.onDidHide(async () => {
+        // this gets called even on accept
+        // and we obviously don't want to jump back to original location
+        // if the user accepted
+        if (!originalDoc || hasAccepted) {
+          return;
+        }
+
+        await window.showTextDocument(originalDoc);
+        originalSelection &&
+          window.activeTextEditor?.revealRange(originalSelection);
+      });
+
+      quickPick.onDidChangeValue(async filter => {
+        quickPick.items = await populateSearchItems(filter);
+      });
+
+      quickPick.onDidChangeSelection(async items => {
+        console.log("selection changed", items);
+        // showDocument(items[0], true);
+      });
+
+      quickPick.onDidChangeActive(async item => {
+        showDocument(item[0], true);
+      });
+
+      quickPick.show();
+    }
+  );
+
+  interface Item extends QuickPickItem {
+    // root path
+    rootpath: string;
+    linenumber: number;
+    text: string;
+    filepath: string;
+  }
+
+  async function showDocument(item?: Item, preserveFocus = false) {
+    if (!item) {
+      return;
+    }
+
+    const docPath = path.join(item.rootpath, item.filepath);
+    const doc = await workspace.openTextDocument(docPath);
+    await window.showTextDocument(doc, { preserveFocus });
+
+    const { activeTextEditor } = window;
+    if (!activeTextEditor) {
+      return;
+    }
+    const pos = new vscode.Position(~~item.linenumber - 1, 0);
+    const range = new vscode.Range(pos, pos);
+    activeTextEditor.selection = new vscode.Selection(pos, pos);
+    activeTextEditor.revealRange(range, vscode.TextEditorRevealType.Default);
+  }
+
+  async function populateSearchItems(filter: string = ""): Promise<Item[]> {
+    function streamToString(stream: Readable): Promise<string> {
+      const chunks: any[] = [];
+      return new Promise((resolve, reject) => {
+        stream.on("data", chunk => chunks.push(Buffer.from(chunk)));
+        stream.on("error", err => reject(err));
+        stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+      });
+    }
+    // TODO best way to get cwd? maybe loop over all workspace roots
+    const cwd = workspace.workspaceFolders![0].uri.fsPath;
+    const child = cp.spawn("/usr/bin/rg", ["-n", "--smart-case", filter], {
+      cwd,
+    });
+    const output = await streamToString(child.stdout);
+    // const errors = await streamToString(child.stderr);
+    // console.log("errors", errors);
+    const lines = output.split("\n");
+
+    // have a reasonable limit before showing anything otherwise it may be slow
+    if (lines.length > LIMIT) {
+      return [];
+    }
+
+    function parse(lines: string[]): Item[] {
+      const parsedLines: Item[] = [];
+      if (!lines) {
+        return parsedLines;
+      }
+
+      for (const line of lines) {
+        const sep = ":";
+        const [filepath, linenumber, ...xs] = line.split(sep);
+        const text = xs.join(sep);
+        if (!text) {
+          continue;
+        }
+        parsedLines.push({
+          // todo what to put in label/description/details?
+          rootpath: cwd,
+          label: "",
+          description: `${filepath}:${linenumber}`,
+          detail: text,
+          text,
+          filepath,
+          linenumber: +linenumber,
+        });
+      }
+
+      return parsedLines;
+    }
+    const parsedLines = parse(lines);
+
+    return parsedLines;
+  }
+
+  context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
