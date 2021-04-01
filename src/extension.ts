4a569804df96772d8efb69a851f57807b8854797
Andy Yu <andyyu2004@gmail.com>
2021-04-01 15:50:08 +1300

    kinda works on windows; using sync code and reads entire rg output without ability to cancel on some limit so might be slow

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as cp from "child_process";
-import { stdout } from "node:process";
 import { Readable } from "node:stream";
 import * as path from "path";
+import { RipGrep } from "ripgrep-node";
 import * as split from "split2";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
@@ -11,6 +11,12 @@ import { QuickPickItem, window, workspace } from "vscode";
 const CONFIG = vscode.workspace.getConfiguration();
 const LIMIT = CONFIG.get<number>("vscode-fzf.resultLimit")!;
 
+// TODO best way to get cwd? maybe loop over all workspace roots
+const cwd = workspace.workspaceFolders?.[0].uri?.fsPath;
+if (!cwd) {
+  window.showErrorMessage("You must be in a workspace to run this command");
+}
+
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "vscode-fzf.rg",
@@ -19,8 +25,8 @@ export function activate(context: vscode.ExtensionContext) {
       quickPick.matchOnDescription = false;
       quickPick.matchOnDetail = true;
       quickPick.placeholder = "Search";
-      const items = await populateSearchItems().catch(console.error);
-      console.log(items);
+      const items = await populateSearchItems();
+      console.log("populateSearch returned", items);
       if (!items) {
         return;
       }
@@ -108,21 +114,56 @@ async function streamToString(stream: Readable): Promise<string> {
 }
 
 async function populateSearchItems(filter: string = ""): Promise<Item[]> {
+  if (process.platform === "win32") {
+    return populateSearchItemsWin(filter);
+  }
+  return populateSearchItemsUnix(filter);
+}
+
+// using alternative implementation for windows as it seems to refuse to
+// capture stdout ('data' event is never called)
+// although stderr seems to work
+// might be rg specific I have no idea anymore
+// however, ripgrep-node seems to work on windows
+// but it uses execSync and I'd rather keep the old async implementation where it works
+async function populateSearchItemsWin(filter: string = ""): Promise<Item[]> {
+  if (!filter) {
+    return [];
+  }
+
+  const useRegex = filter.startsWith("/");
+  if (useRegex) {
+    filter = filter.substring(1);
+  }
+
+  console.log(filter);
+  const rg = new RipGrep(filter, cwd!).lineNumber().smartCase().maxCount(10);
+  if (!useRegex) {
+    rg.fixedStrings();
+  }
+  rg.run();
+
+  const output = rg.asString();
+
+  if (output.length > 10000) {
+    return [];
+  }
+
+  return <Item[]>output
+    .split("\n")
+    .map(parseLine)
+    .filter(item => item !== undefined);
+}
+
+async function populateSearchItemsUnix(filter: string = ""): Promise<Item[]> {
   return new Promise(async (resolve, reject) => {
     if (!filter) {
       return resolve([]);
     }
 
-    // TODO best way to get cwd? maybe loop over all workspace roots
-    const cwd = workspace.workspaceFolders?.[0].uri?.fsPath;
-    if (!cwd) {
-      window.showErrorMessage("You must be in a workspace to run this command");
-      return reject(undefined);
-    }
-
     // using a pretty arbitrary idea that a leading `/` indicates regex
-    const useRegex = filter.startsWith("/");
     const args = ["--line-number", "--smart-case"];
+    const useRegex = filter.startsWith("/");
     if (useRegex) {
       filter = filter.substring(1);
     } else {
@@ -131,46 +172,32 @@ async function populateSearchItems(filter: string = ""): Promise<Item[]> {
     args.push(filter);
 
     const rgExecPath = "rg";
-    console.log(cwd);
+
     const child = cp.spawn(rgExecPath, args, {
       cwd,
+      stdio: "pipe",
       // this is necessary to make it work on windows
       // but works fine without on linux and osx
-      // shell: process.platform === "win32",
-      shell: true,
+      shell: process.platform === "win32",
     });
 
     child.on("error", () => console.log("failed to spawn rg"));
 
-    const stderr = await streamToString(child.stderr);
-    console.log("stderr", stderr);
+    // const stderr = await streamToString(child.stderr);
+    // console.log("stderr", stderr);
     // const stdout = await streamToString(child.stdout);
     // console.log("stdout", stdout);
 
     const parsedLines: Item[] = [];
+
     const stream = child.stdout.pipe(split());
     stream.on("close", () => resolve(parsedLines));
     stream.on("end", () => resolve(parsedLines));
     stream.on("error", () => reject("stream failed"));
+    stream.on("readable", () => console.log("stdout is readable"));
     stream.on("data", line => {
-      console.log("recv data");
-      const sep = ":";
-      const [filepath, linenumber, ...xs] = line.split(sep);
-      const text = xs.join(sep);
-      if (!text) {
-        return;
-      }
-
-      parsedLines.push({
-        // todo what to put in label/description/details?
-        rootpath: cwd,
-        label: "",
-        description: `${filepath}:${linenumber}`,
-        detail: text,
-        text,
-        filepath,
-        linenumber: +linenumber,
-      });
+      const item = parseLine(line);
+      item && parsedLines.push(item);
 
       if (parsedLines.length >= LIMIT && !stream.destroyed) {
         stream.destroy();
@@ -178,6 +205,25 @@ async function populateSearchItems(filter: string = ""): Promise<Item[]> {
       }
     });
   });
+}
+
+function parseLine(line: string): Item | undefined {
+  const sep = ":";
+  const [filepath, linenumber, ...xs] = line.split(sep);
+  const text = xs.join(sep);
+  if (!text) {
+    return;
+  }
+  return {
+    // todo what to put in label/description/details?
+    rootpath: cwd!,
+    label: "",
+    description: `${filepath}:${linenumber}`,
+    detail: text,
+    text,
+    filepath,
+    linenumber: +linenumber,
+  };
 }
 
 // this method is called when your extension is deactivated
